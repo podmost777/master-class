67fb865dea63f4eff5b5fb231e757aa192398724
Andrey <57412446+podmost777@users.noreply.github.com>
2020-01-24 14:33:26 +0300

    Modifided files

--- src/index.js
+++ src/index.js
@@ -1,57 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-
-const movie={
-  title:" Avengers : Infinity War",
-  vote_average: 8.5,
-  image :"https://thypix.com/wp-content/uploads/2018/05/Sommerlandschaft-Bilder-99.jpg",
-  overview: "As the",
-};
-
-function Image(props){
-  console.log("Imagerops", props);
-  return <img src={props.src} alt={props.alt}/>;
-  
-}
-
-// function MovieItem(props){
-
-//     console.log(" MovieItemprops=",  props)
-//     const { data: {title, vote_average, image} } = props;
-//     return (
-//       <div>
-//         <img src={image} alt={title}/>
-//       <h1>{title}</h1>
-//       <p>{vote_average}</p>
-        
-//       </div>
-//     );
-//   }
-
-  class MovieItem extends React.Component{
-    render(){
-      const { data: {title, vote_average, image} } = this.props;
-      console.log(this);
-     return (
-      <div>
-         <img src={image} alt={title}/>
-         <h1>{title}</h1>
-         <p>{vote_average}</p> 
-      </div>
-
-      );
-    }
-  }
-  
-  function App() {
-    return (
-      
-       <div>
-         <MovieItem data={movie}/>
-       </div>
-
-    );
-  }
+import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
